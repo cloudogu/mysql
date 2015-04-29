8d38a3f607faeedce907b490f1b44e8521606736
Sebastian Sdorra <sebastian.sdorra@triology.de>
2015-04-29 14:22:37 +0200

    implement new structure for containers

---
+++ containers/mysql/create.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+dbDir="/var/lib/ces/mysql"
+
+if ! [ -d $dbDir ]; then
+	btrfs subvolume create $dbDir
+	if ! [ -d $dbDir/data ]; then
+		mkdir $dbDir/data
+		chown 1000:1000 $dbDir/data
+	fi
+fi
+
+docker create --name mysql -h mysql -v /etc/ces:/etc/ces -v $dbDir/data:/var/lib/mysql cesi/mysql
