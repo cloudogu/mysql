1183caf21ca30f94b3eb8a4f60fa121a9b0e563e
marco.bergen <marco.bergen-extern@cloudgu.com>
2024-10-07 12:13:59 +0200

    update to 8.4.2

--- installation-scripts/install-mysql.sh
+++ installation-scripts/install-mysql.sh
@@ -2,18 +2,18 @@
 set -o errexit
 set -o nounset
 set -o pipefail
-DEBIAN_SHA_256_SUM="a62bca0a0fd67e11fd5c8efde7e67e6e59255c3f0fa61ecc817fd99254b483ab"
-VERSION="0.8.29-1"
+DEBIAN_SHA_256_SUM="df9c563abd70bb9b2fb1be7d11868a300bd60023bcd60700f24430008059a704"
+VERSION="0.8.32-1"
 # see for latest version: https://dev.mysql.com/downloads/repo/apt/
 wget "https://dev.mysql.com/get/mysql-apt-config_${VERSION}_all.deb"
 echo "${DEBIAN_SHA_256_SUM} mysql-apt-config_${VERSION}_all.deb" | sha256sum -c -
 # Select the correct mysql package
 # 1. '1': Select the option to choose the mysql version
-# 2. '1': Select mysql8.0
+# 2. '3': Select mysql8.4-lts
 # 3. 'ok': Finish configuration
 dpkg -i "mysql-apt-config_${VERSION}_all.deb" <<EOF
 1
-1
+3
 ok
 EOF
 # https://repo.mysql.com/apt/ubuntu/conf/distributions
